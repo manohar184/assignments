29bcd08eff2ba9b17ef8ed1394379e8b928c06ed
manohar <manohar18.marolix@gmail.com>
2023-05-16 15:26:51 +0530

    initial commit

---
+++ 16.05.2023/userCkecking.py
@@ -0,0 +1,8 @@
+name=" "
+password= " "
+
+while name !="Manohar" and password !="manohar.marolix":
+    name= input("enter your name to validate it:")
+    password= input("enter your password to validate it:")
+    
+print("Hi Manohar, welcome to Marolix")    
